c2bbab151e779474fadc0ee8ebc5e079a68c42cc
jsm8109jsm <jsm8109jsm@gmail.com>
2022-12-31 22:13:20 +0900

    큐

---
+++ python/큐.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+
+n = int(input())
+
+q = deque()
+
+for i in range(n):
+  a = sys.stdin.readline().rstrip()
+  if a == 'pop':
+    if q:
+      print(q.popleft())
+    else:
+      print(-1)
+  elif a == 'size':
+    print(len(q))
+  elif a == 'empty':
+    if q:
+      print(0)
+    else:
+      print(1)
+  elif a == 'front':
+    if q:
+      print(q[0])
+    else:
+      print(-1)
+  elif a == 'back':
+    if q:
+      print(q[len(q) - 1])
+    else:
+      print(-1)
+  else:
+    push = a.split(' ')
+    q.append(int(push[1]))
