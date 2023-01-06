376b6de40bda051a6d9c74aac1633e92ff27fc92
jsm8109jsm <jsm8109jsm@gmail.com>
2023-01-06 14:56:00 +0900

    백준 1026 보물

---
+++ python/보물.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+s = 0
+
+for i in range(0, n):
+    s += a[i] * b[i]
+
+print(s)
